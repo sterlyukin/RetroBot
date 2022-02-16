5285984725d49f47470dab99f56097036167697a
sterlyukin <sterlyukinnikita@gmail.com>
2022-02-16 19:38:44 +0300

    Remove test code from command handler

--- src/RetroBot.Application/CommandHandlers/StartCommandHandler.cs
+++ src/RetroBot.Application/CommandHandlers/StartCommandHandler.cs
@@ -21,7 +21,6 @@ internal sealed class StartCommandHandler : CommandHandler
         var contactName = GetContactName(info);
         var greetingMessage = string.Format(messages.Greeting, contactName);
 
-        var questions = await storage.TryGetQuestionsAsync();
         var user = await storage.TryGetByUserIdAsync(info.Message.From.Id);
         if (user is not null)
         {
